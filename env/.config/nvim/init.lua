7443298026a827126e1fd683f20b6bd275d00b13
Luke Berry <lukeberry99@gmail.com>
2025-02-08 02:20:11 +0000

    feat: install script and some base configs

---
+++ env/.config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.options")
+require("config.keys")
+require("config.autocommands")
+require("config.lazy")
---
+++ env/.config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,45 @@
+return {
+	{ "tpope/vim-sensible" },
+	{ "tpope/vim-sleuth" },
+	{ "ibhagwan/fzf-lua" },
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
+
+		version = "v0.*",
+
+		opts = {
+			keymap = { preset = "default" },
+
+			appearance = {
+				use_nvim_cmp_as_default = true,
+				nerd_font_variant = "mono",
+			},
+
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+	},
+}
