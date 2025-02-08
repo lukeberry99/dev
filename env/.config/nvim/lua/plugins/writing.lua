770d90f4e0e88f92affa18d16065cfb0a8f108f8
Luke Berry <lukeberry99@gmail.com>
2025-02-08 11:19:25 +0000

    automated dev commit

--- env/.config/nvim/lua/plugins/writing.lua
+++ env/.config/nvim/lua/plugins/writing.lua
@@ -12,32 +12,13 @@ return {
 						path = "~/vault",
 					},
 				},
-				notes_subdir = "inbox",
-				new_notes_location = "notes_subdir",
+				notes_subdir = ".",
+				new_notes_location = ".",
+				new_note_filename = function(title)
+					return title:gsub(" ", "_") -- Replace spaces with underscores
+				end,
 
-				disable_frontmatter = true,
-				templates = {
-					subdir = "templates",
-					date_format = "%Y-%m-%d",
-					time_format = "%H:%M:%S",
-				},
-
-				-- name new notes starting the ISO datetime and ending with note name
-				-- put them in the inbox subdir
-				-- note_id_func = function(title)
-				--   local suffix = ""
-				--   -- get current ISO datetime with -5 hour offset from UTC for EST
-				--   local current_datetime = os.date("!%Y-%m-%d-%H%M%S", os.time() - 5*3600)
-				--   if title ~= nil then
-				--     suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-				--   else
-				--     for _ = 1, 4 do
-				--       suffix = suffix .. string.char(math.random(65, 90))
-				--     end
-				--   end
-				--   return current_datetime .. "_" .. suffix
-				-- end,
-
+				disable_frontmatter = false,
 				-- key mappings, below are the defaults
 				mappings = {
 					-- overrides the 'gf' mapping to work on markdown/wiki links within your vault
--- env/.zshrc
+++ env/.zshrc
@@ -3,3 +3,4 @@ eval "$(starship init zsh)"
 eval "$(fnm env --use-on-cd --shell zsh)"
 
 alias dev="cd $HOME/repos/github.com/lukeberry99/dev"
+alias vault="cd $HOME/vault"
