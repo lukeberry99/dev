a54c3b496916be02c7b86669dd5942579fed14d1
Luke Berry <lukeberry99@gmail.com>
2025-04-05 12:10:27 +0100

    [2025-04-05 12:10:27] Automated dev commit

--- env/.config/nvim/lua/plugins/coding.lua
+++ env/.config/nvim/lua/plugins/coding.lua
@@ -45,4 +45,19 @@ return {
 		ft = { "go", "gomod" },
 		build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
 	},
+	{
+		"nvim-java/nvim-java",
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	},
 }
