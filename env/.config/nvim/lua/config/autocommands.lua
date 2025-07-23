8d7cc664d924122e29b143de20b384bc4ebb7c62
Luke Berry <lukeberry99@gmail.com>
2025-07-23 19:46:15 +0100

    [2025-07-23 19:46:15] Automated dev commit

--- env/.config/nvim/lua/config/autocommands.lua
+++ env/.config/nvim/lua/config/autocommands.lua
@@ -1,7 +1,17 @@
 vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking text",
-  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "markdown",
+	callback = function()
+		vim.opt_local.wrap = true -- enables visual line wrapping
+		vim.opt_local.linebreak = true -- wraps at word boundaries
+		vim.opt_local.colorcolumn = "80" -- visual guide at 80 chars
+		vim.opt_local.textwidth = 80 -- hard wrap via formatting
+	end,
 })
